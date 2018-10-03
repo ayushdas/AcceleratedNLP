d09a3cbbc287f2eb284852378290cff1a99387af
Ayush Das <ayushdas@c103148.wlan.net.ed.ac.uk>
2018-10-03 17:56:55 +0100

    Pre-processing completed

--- Assignment-1/assignment1-code/asgn1.py
+++ Assignment-1/assignment1-code/asgn1.py
@@ -8,11 +8,32 @@ from collections import defaultdict
 
 tri_counts=defaultdict(int) #counts of all trigrams in input
 
+# Check for English Character
+def isEngAlpha(character):
+    if (character >= 'a' and character <= 'z'):
+        return True
+    elif (character >= 'A' and character <= 'Z'):
+        return True
+    else:
+        return False
 
-#this function currently does nothing.
+#Pre Processes the input line: by keeping only certain caharcters in the set {alpha[lowercase],space,isdidgit,.}
 def preprocess_line(line):
-    return line
+    character_list = list()
+    for character in line:
+        if (isEngAlpha(character)): # character.isalpha(): True for other language characters also, hence use isEngAlpha()
+            character_list.append(character.lower()) # keep lower characters
+        elif (character.isspace()): 
+            character_list.append(character) # keep ' '
+        elif (character.isdigit()):
+            character_list.append('0') # convert digits {0-9} to 0
+        elif (character == "."):
+            character_list.append(character) # keep '.'
+        else:
+            character_list.append('')   
 
+    line = "".join(character_list)      
+    return line
 
 #here we make sure the user provides a training filename when
 #calling this program, otherwise exit with a usage error.
@@ -27,9 +48,12 @@ infile = sys.argv[1] #get input argument: the training file
 #please ensure you understand what it is actually doing, especially at the
 #beginning and end of each line. Depending on how you write the rest of
 #your program, you may need to modify this code.
+
+
 with open(infile) as f:
     for line in f:
-        line = preprocess_line(line) #doesn't do anything yet.
+        line = preprocess_line(line) # Pre Processes the input line
+        print(line)
         for j in range(len(line)-(3)):
             trigram = line[j:j+3]
             tri_counts[trigram] += 1
@@ -37,6 +61,7 @@ with open(infile) as f:
 #Some example code that prints out the counts. For small input files
 #the counts are easy to look at but for larger files you can redirect
 #to an output file (see Lab 1).
+
 print("Trigram counts in ", infile, ", sorted alphabetically:")
 for trigram in sorted(tri_counts.keys()):
     print(trigram, ": ", tri_counts[trigram])
