52e72f31985cca5f02adcfc2ee8534fc24a432cf
Matthew Aquilina <matthewaq31@gmail.com>
2018-10-05 16:00:09 +0100

    Finalised Perplexity

--- Assignment-1/assignment1-code/asgnv2.py
+++ Assignment-1/assignment1-code/asgnv2.py
@@ -2,7 +2,8 @@ import re
 import sys
 import random
 # from random import random
-from math import log
+from math import log10
+from math import pow
 from collections import defaultdict
 import linecache
 
@@ -47,8 +48,9 @@ def read_model_from_file(file):
     in_model = defaultdict(int)
     with open(file) as f:
         for line in f:
+            # print (line)
             (key, val) = line.split('\t')
-            in_model[key] = val.rstrip('\n')
+            in_model[key] = float(val.rstrip('\n')) ## Convert values to float
     return in_model
 
 def generate_debug_filepath(model):
@@ -78,7 +80,7 @@ def testing_routine(line_num,infile):
     print ('PROCESSED LINE:')
     print (processed_line)
     find_bi_tri_grams(processed_line)
-    provided_model = read_model_from_file('../assignment1-models/model-br.en')
+    # provided_model = read_model_from_file('../assignment1-models/model-br.en')
     
 def complete_model(infile):
     with open(infile) as f:
@@ -107,19 +109,49 @@ def trigram_viewer(alpha,num,infile):
             print(tri_count[0], ": ", str(tri_count[1]))
 
 def perplexity_computation(file,model):
-
     with open(file) as f:
         total_prob = 0
         total_tris = 0
+        trigram_prob = 0
         for line in f:
             processed_line = preprocess_line(line)
+            # processed_line = line
             for j in range(len(processed_line)-(2)):
                 total_tris +=1
                 trigram = processed_line[j:j+3]
-                trigram_prob = log(model[trigram],10)
-                total_prob = trigram_prob + total_prob
-        # perplexity = 
-        # take antilog and do to power of 1/n?  still unsure here....
+                # print (trigram)
+                if trigram in model.keys():
+                    trigram_prob = log10(model[trigram])
+                else:
+                    print('unseen trigram: ' + trigram)
+                total_prob += trigram_prob 
+        log_perplexity = total_prob*(-1/total_tris)
+        perplexity = pow(10,log_perplexity)
+    return perplexity
+
+def init_dummy_model():
+    distribution = dict([('##a', 0.2),
+                    ('#aa', 0.2),
+                    ('#ba', 0.15),
+                    ('aaa', 0.4),
+                    ('aba', 0.6),
+                    ('baa', 0.25),
+                    ('bba', 0.5),
+                    ('##b', 0.8),
+                    ('#ab', 0.7),
+                    ('#bb', 0.75),
+                    ('aab', 0.5),
+                    ('abb', 0.3),
+                    ('bab', 0.65),
+                    ('bbb', 0.4),
+                    ('###', 0.0),
+                    ('#a#', 0.1),
+                    ('#b#', 0.1),
+                    ('aa#', 0.1),
+                    ('ab#', 0.1),
+                    ('ba#', 0.1),
+                    ('bb#', 0.1)])
+    return distribution
 
 def trigram_with_two_character_history(char1,char2,tri_probs):
     prefix = char1+char2
@@ -127,7 +159,7 @@ def trigram_with_two_character_history(char1,char2,tri_probs):
     for key in  tri_probs:
         if (key.startswith(prefix)):
             print ('n-gram','\t',key,'\t',tri_probs[key])
-    return            
+                
 def generate_from_LM(num_of_chars,tri_probs):
     valid_char_list = [' ','.','0']
     for i in range(ord('a'),ord('z')+1):
@@ -144,7 +176,7 @@ def generate_from_LM(num_of_chars,tri_probs):
         num_of_iter = 0
         while(len(seq) != Num_Of_Chars and num_of_iter <= 1000):  
             two_char_seq = (random.choice(valid_char_list))+(random.choice(valid_char_list))
-            print(two_char_seq)    
+            # print(two_char_seq)    
             seq = two_char_seq
             num_of_chars = Num_Of_Chars-2
             while (num_of_chars > 0):
@@ -169,32 +201,57 @@ def generate_from_LM(num_of_chars,tri_probs):
     return seq
 
 def main_routine():
-
-    debugger = True
-    testing = True
+    # Parameter selection
+    debugger = False
+    testing = False
+    modelling = True
     alpha = False
     num = False
     line_num = 4
     model_lang = 'en'
     test_file = '../assignment1-data/test'
+    model_file = '../assignment1-models/Empirical_Model_en'
+    given_model_file = '../assignment1-models/model-br.en'
 
     if debugger:
         infile = generate_debug_filepath('training.'+model_lang)
     else:
         infile = terminal_input_filepath()
 
-    if testing:
-        testing_routine(line_num,infile)
-        # save_model_to_file(bi_counts,'testline.txt')
+    if modelling:
+        # model_in = init_dummy_model()
+        model_in = read_model_from_file(model_file)
+        print ('Perplexity: ' + str(perplexity_computation('../assignment1-data/training.de',model_in)))
     else:
-        complete_model(infile)
-        tri_probs = estimate_probs(bi_counts,tri_counts)
-        save_model_to_file(tri_probs,'modelv1.txt')
-        sequence = generate_from_LM(300,tri_probs)
-        perplexity_computation(test_file,tri_probs)
+        if testing:
+            testing_routine(line_num,infile)
+            # save_model_to_file(bi_counts,'testline.txt')
+        else:
+            print('Question 1:')
+            print('----------------------')
+            complete_model(infile)
+            print('Preprocessing complete')
+            tri_probs = estimate_probs(bi_counts,tri_counts)
+            save_model_to_file(tri_probs,'../assignment1-models/Empirical_Model_' + model_lang)
+            print('Question 3:')
+            print('----------------------')
+            print('All trigams starting with "ng":')
+            trigram_with_two_character_history('n','g',tri_probs)
+            print('Question 4:')
+            print('----------------------')
+            given_model = read_model_from_file(given_model_file)
+            sequence = generate_from_LM(300,tri_probs)
+            print('Our model:')
+            print (sequence) #sequence 
+            sequence = generate_from_LM(300,given_model)
+            print ('Given model:')
+            print (sequence)
+            print('Question 5:')
+            print('----------------------')
+            print ('Perplexity of file: ' +str(perplexity_computation(test_file,tri_probs)))
 
-    # bigram_viewer(alpha,num,infile)
-    trigram_viewer(alpha,num,infile)
+        # bigram_viewer(alpha,num,infile)
+        # trigram_viewer(alpha,num,infile)
 
 if __name__ == '__main__':
     main_routine()
